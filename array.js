6fa1e6ff5d0cd769caf9a1a316262f940c9125fe
bukola-radease <bukola.radease@gmail.com>
2025-09-21 21:29:32 +0100

    arrays and function

--- array.js
+++ array.js
@@ -5,15 +5,21 @@ console.log(numbers)
 const employeeList = [
     {
         id: 1,
-        name: 'jim'
+        name: 'jim',
+        role: 'developer',
+        email: 'JIM@gmail.com'
     },
      {
         id: 2,
-        name: 'jide'
+        name: 'jide',
+        role: "driver",
+        email: 'JIDE@gmail.com'
     },
      {
         id: 3,
-        name: 'jite'
+        name: 'jite',
+        role: "developer",
+        email: 'JITE@gmail.com'
     }
 ]
 
@@ -34,7 +40,8 @@ const array1 = ['ola', 'ade', 'ayo']
 const array2 = ['tolu', 'lope', 'bola']
 
 const combinedArray = array1.concat(array2)
-console.log(combinedArray.splice(1,4))
+console.log( combinedArray)
+console.log( "combined result" + "" +combinedArray.slice(1,4))
 
 const spreadArray = [...array1, array2]
 
@@ -56,3 +63,62 @@ console.log(split_Sentence)
 const sortedArray = array1.sort()
 console.log(sortedArray)
 
+//every method
+
+const num = [2,4,6,8,10];
+
+const areAllEven = num.every(n => n % 2 === 0);
+
+console.log(`are all even : ${areAllEven}`)
+
+
+//some method - returns true if a certain element in the array fulfils the required parameter
+const oneEvenNum = [10, 13,17,19,37]
+const hasOneEvenNum = oneEvenNum.some(evenNum => evenNum % 2 === 0)
+
+console.log(`has one even number : ${hasOneEvenNum}`)
+
+//filter method
+
+const arrayNum = [1,2,3,4,5,6,7,8,9]
+const filterEvenNum = arrayNum.filter(en =>{
+    return en % 2 === 0
+})
+
+console.log(`filtered array : ${filterEvenNum}`)
+
+const devRole = employeeList.filter(employee => employee.role === 'developer')
+
+console.log(devRole)
+
+//map method
+
+const squardNum = numbers.map(nm => nm*nm)
+console.log(a=squardNum)
+console.log(numbers)
+
+const charcters = ["a", "b", "c", "d"]
+const mapToUpperCase = charcters.map(ch => ch.toLocaleUpperCase())
+console.log(mapToUpperCase)
+
+const mappedEmploye = employeeList.map(em => ({
+    ...em,
+    email: em.email.toLocaleLowerCase()
+}) )
+
+console.log(mappedEmploye)
+
+ //reduvced array
+
+ const reducednumbers = [10,23,45,67]
+//  let sum = 0
+
+//  for (const rn of reducednumbers)
+//     sum += rn
+// console.log(sum)
+
+const reducedArray = reducednumbers.reduce((accumulator, currentvalue)=>{
+    return accumulator + currentvalue
+})
+
+console.log(reducedArray)
---
+++ functions.js
@@ -0,0 +1,12 @@
+//function declaration
+function sayHi (){
+console.log ("hi")
+}
+
+sayHi()
+// function expression
+const greeeting = function () {
+    console.log('hello')
+};
+
+greeeting()
