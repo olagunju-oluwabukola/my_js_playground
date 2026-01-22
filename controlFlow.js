8b0fe6c4210289a8113109aa60b0e1b83c0f13da
olagunju-oluwabukola <olagunjuoluwabukola2018@gmail.com>
2026-01-22 11:32:47 +0100

    control flows

---
+++ controlFlow.js
@@ -0,0 +1,21 @@
+const point = 110
+const customerType = point > 100 ? "Gold" : "silver"
+console.log(customerType)
+
+const c1 = "blue"
+const c2 = "red"
+const truthy = c1 && c2
+console.log(truthy)
+
+
+let job = "developer"
+
+switch (job) {
+    case "fashion designer " :
+        console.log("sew cloth")
+        case "cloud eng":
+            console.log.log("manage cloud architects")
+            case "developer":
+                console.log("builds solution")
+                default: ("can be anything")
+}
